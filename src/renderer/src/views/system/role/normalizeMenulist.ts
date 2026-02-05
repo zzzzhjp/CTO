6ffe2a42cc2cbcb764d2147def795e08248ad1de
张君鹏 <1691876157@qq.com>
2026-02-05 23:45:17 +0800

    修复树形结构转换问题

--- src/renderer/src/views/system/role/normalizeMenulist.ts
+++ src/renderer/src/views/system/role/normalizeMenulist.ts
@@ -1,15 +1,15 @@
 import { IRoleTree  } from '@api/role'
 const normalizeMenuList = ( list:IRoleTree[] , cb:(menu:IRoleTree)=>void =()=>null):IRoleTree[]=>{
-    const menuMap = new Map(list.map(menu=>[menu.id,menu]));
+    const menuMap = new Map(list.map(menu=>[menu.id,{...menu, children: []}]));
     const result:IRoleTree[] = [];
 
     list.forEach((menu:IRoleTree)=>{
         cb(menu);
-        if( menu.parentId =='-1' ){
-            return result.push( menu );
+        if( menu.parentId === '-1' || menu.parentId === '0' || menu.parentId === 0 || !menu.parentId ){
+            return result.push( menuMap.get(menu.id)! );
         }
-        const parent = menuMap.get( menu.parentId );
-        parent && parent.children?.push( menu );
+        const parent = menuMap.get( menu.parentId.toString() );
+        parent && parent.children?.push( menuMap.get(menu.id)! );
         return ;
     })
     sortMenuList( result );
